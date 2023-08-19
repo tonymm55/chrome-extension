1fe4320e38de76fbbb56b5b7ee35295e745ea5bd
TonyMM55 <tonymmoran@gmail.com>
2023-08-19 16:20:35 +0100

    adding entries to a rendered list using innerHTML

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-// Get references to the input element and the button
-let inputEl = document.getElementById('input-el');
-const inputButton = document.getElementById('input-btn');
-const myLeads = [];
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+})
 
-// Add an event listener to the button
-inputButton.addEventListener('click', function() {
-    console.log('Button clicked!'); // Log the message when the button is clicked
-});
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
+let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
+    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+    listItems += "<li>" + myLeads[i] + "</li>"
+    console.log(listItems)
+}
+// 3. Render the listItems inside the unordered list using ulEl.innerHTML
+ulEl.innerHTML = listItems
