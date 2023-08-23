99fdc70a1642d5a3f1540fe7a925c6b0a4096ccf
TonyMM55 <tonymmoran@gmail.com>
2023-08-23 14:15:19 +0100

    added ability to save tabs in Chrome

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
-
 tabBtn.addEventListener("click", function() {
-    console.log(tabs[0].url)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
 function render(leads) {
@@ -47,8 +47,4 @@ inputBtn.addEventListener("click", function() {
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads)
-    console.log(localStorage.getItem("myLeads"))
-
 })
-
-
